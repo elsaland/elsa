45828a2d03491f7af978219c8887de5ae8c0adb4
divy-work <dj.srivastava23@gmail.com>
2020-09-18 16:32:59 +0530

    chores: change testdata console usage

--- testdata/hello.js
+++ testdata/hello.js
@@ -3,11 +3,11 @@ function test() {
   return "test";
 }
 
-__console_write("Done 1");
+console.log("Done 1");
 
-__console_write("Done 2");
+console.log("Done 2");
 
-__console_write("Done 3");
+console.log("Done 3");
 
 const arrow = () => {
   return 1;
