1cce0935a6231ba8c1c6ff2675ea7f8b58e753df
divy-work <dj.srivastava23@gmail.com>
2020-09-20 19:55:30 +0530

    chores: refract ns

---
+++ js/00_opcodes.js
@@ -0,0 +1,7 @@
+// Define op codes
+const __ops = {
+    FSRead: 1,
+    FSWrite: 2,
+    
+    Log: 3,
+}
---
+++ js/01_namespace.js
@@ -0,0 +1,27 @@
+
+globalThis.Done = {
+    readFile: (arg) => {
+        return globalThis.__dispatch(__ops.FSRead, arg);
+    }
+};
+
+globalThis.console = {
+    trace: (...args) => {
+        globalThis.__dispatch(__ops.Log, ...args);
+    },
+    debug: (...args) => {
+        globalThis.__dispatch(__ops.Log, ...args);
+    },
+    log: (...args) => {
+        globalThis.__dispatch(__ops.Log, ...args);
+    },
+    info: (...args) => {
+        globalThis.__dispatch(__ops.Log, ...args);
+    },
+    warn: (...args) => {
+        globalThis.__dispatch(__ops.Log, ...args);
+    },
+    error: (...args) => {
+        globalThis.__dispatch(__ops.Log, ...args);
+    },
+};
