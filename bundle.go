b72983038fbbf8516a03d727c162d00aac327e9a
Divy Srivastava <dj.srivastava23@gmail.com>
2020-09-22 20:42:55 +0530

    feat: add url caching algorigthm (#4)

--- bundle.go
+++ bundle.go
@@ -21,28 +21,33 @@ func BundleModule(source string) string {
 		Bundle:      true,
 		Target:      api.ESNext,
 		LogLevel:    api.LogLevelInfo,
-		// Write:       true,
 		Plugins: []func(api.Plugin){
 
 			func(plugin api.Plugin) {
 				plugin.SetName("url-loader")
 				plugin.AddResolver(api.ResolverOptions{Filter: "^https?://"},
 					func(args api.ResolverArgs) (api.ResolverResult, error) {
-						LogInfo("Downloading", args.Path)
-						// Get the data
-						resp, _ := http.Get(args.Path)
-						fileName := buildFileName(args.Path)
-						defer resp.Body.Close()
-						file, err := create(fileName)
-						if err != nil {
-							LogError("Internal", fmt.Sprintf("%s", err))
-							os.Exit(1)
-						}
-						io.Copy(file, resp.Body)
+						possibleCachePath := urlToPath(args.Path)
+						if inCache(possibleCachePath) && exists(possibleCachePath) {
+							return api.ResolverResult{Path: possibleCachePath, Namespace: "url-loader"}, nil
+						} else {
+							LogInfo("Downloading", args.Path)
+							// Get the data
+							resp, _ := http.Get(args.Path)
+							fileName := buildFileName(args.Path)
+							defer resp.Body.Close()
+							file, err := create(fileName)
+							if err != nil {
+								LogError("Internal", fmt.Sprintf("%s", err))
+								os.Exit(1)
+							}
+							io.Copy(file, resp.Body)
 
-						defer file.Close()
-						LogInfo("Downloaded", file.Name())
-						return api.ResolverResult{Path: file.Name(), Namespace: "url-loader"}, nil
+							defer file.Close()
+							LogInfo("Downloaded", file.Name())
+							return api.ResolverResult{Path: file.Name(), Namespace: "url-loader"}, nil
+						}
+
 					})
 				plugin.AddLoader(api.LoaderOptions{Filter: ".*?", Namespace: "url-loader"},
 					func(args api.LoaderArgs) (api.LoaderResult, error) {
@@ -63,7 +68,7 @@ func BundleModule(source string) string {
 							p = file.Name()
 							LogInfo("Downloaded", file.Name())
 						}
-						LogInfo("Loading ", p)
+						LogInfo("Loading", p)
 						dat, e := ioutil.ReadFile(p)
 						if e != nil {
 							panic(e)
@@ -88,6 +93,12 @@ func pathToUrl(path string) string {
 	parts := strings.Split(path, "/")[2:]
 	url, _ := url.Parse("https://" + strings.Join(parts, "/"))
 	return url.String()
+}
+
+func urlToPath(url string) string {
+	parts := strings.Split(url, "//")[1]
+	cache := path.Join(os.TempDir(), parts)
+	return cache
 }
 
 func inCache(path string) bool {
