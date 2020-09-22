bbdd10c2b7c5b6f4f3d5b92310da3c2bf7f87bf5
divy-work <dj.srivastava23@gmail.com>
2020-09-22 16:29:33 +0530

    chores: update ci

--- bootstrap/snap.go
+++ bootstrap/snap.go
@@ -13,7 +13,7 @@ func main() {
 
 	sort.Sort(ByNumericalFilename(files))
 
-	binCmd := []string{"-o", "data.go", "typescript/", "target/"}
+	binCmd := []string{"run", "github.com/go-bindata/go-bindata", "-o", "data.go", "typescript/", "target/"}
 	var finalSource string
 	for _, f := range files {
 		log.Printf("Bundling %s\n", f.Name())
@@ -22,7 +22,7 @@ func main() {
 	}
 	os.Mkdir("target/", 0777)
 	ioutil.WriteFile("target/elsa.js", []byte(finalSource), 0644)
-	cmd := exec.Command("go-bindata", binCmd...)
+	cmd := exec.Command("go", binCmd...)
 	log.Printf("Running command and waiting for it to finish...")
 	err := cmd.Run()
 	log.Printf("Command finished with error: %v", err)
