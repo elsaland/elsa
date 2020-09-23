429cf9d1eb1dcdf2d0aecd9e1193a637508faa3b
Filippo Rossi <12383260+qu4k@users.noreply.github.com>
2020-09-23 04:04:29 +0200

    Prepare project for contribution (#6)
    * chore: quick formatting and import refactor
    
    * chore: reload sum

--- cache.go
+++ cache.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-  "net/url"
-  "os"
-  "path"
+	"net/url"
+	"os"
+	"path"
 	"path/filepath"
-  "strings"
+	"strings"
 )
 
 type ElsaCache struct {
-  dir string
+	dir string
 }
 
 func (cache *ElsaCache) BuildFileName(uri string) string {
-  fileUrl, _ := url.Parse(uri)
-  path := path.Join(cache.dir, fileUrl.Host, fileUrl.Path)
-  return path
+	fileUrl, _ := url.Parse(uri)
+	path := path.Join(cache.dir, fileUrl.Host, fileUrl.Path)
+	return path
 }
 
 func (cache *ElsaCache) PathToUrl(path string) string {
-  parts := strings.Split(path, "/")[2:]
+	parts := strings.Split(path, "/")[2:]
 	url, _ := url.Parse("https://" + strings.Join(parts, "/"))
 	return url.String()
 }
 
 func (cache *ElsaCache) UrlToPath(url string) string {
-  parts := strings.Split(url, "//")[1]
+	parts := strings.Split(url, "//")[1]
 	path := path.Join(cache.dir, parts)
 	return path
 }
