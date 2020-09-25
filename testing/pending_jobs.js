d53ccf135d86a11d9a106fc486c78c6d69492d9e
Qu4k <pippo.reds@gmail.com>
2020-09-25 13:23:34 +0200

    feat: implement pending job resolution (#22)
    Closes #22

---
+++ testing/pending_jobs.js
@@ -0,0 +1,14 @@
+// TODO(@qu4k): remove when tla is supported
+(async () => {
+  const promise = new Promise(resolve => {
+    resolve(anotherHello());
+  });
+  console.log(await (await promise));
+})();
+
+async function anotherHello() {
+  const promise = new Promise(resolve => {
+    resolve("Hello World");
+  });
+  return promise;
+}
