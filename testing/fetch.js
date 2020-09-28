aa440317d1185a174181f4145aad0f1cb6714ee4
divy-work <dj.srivastava23@gmail.com>
2020-09-28 12:14:17 +0530

    chores: add more fetch examples

--- testing/fetch.js
+++ testing/fetch.js
@@ -3,3 +3,11 @@ fetch("https://google.com").then(res => {
   console.log(res)
 })
 
+fetch("https://divy.work").then(res => {
+  console.log(res)
+})
+
+fetch("https://nest.land").then(res => {
+  console.log(res)
+})
+
