a0f3e581410171034b9e4558a29c4d92cb32577e
divy-work <dj.srivastava23@gmail.com>
2020-10-02 21:08:10 +0530

    fix tests for pkg

--- packager/exec.go
+++ packager/exec.go
@@ -1,13 +1,22 @@
 package packager
 
 import (
-	"github.com/elsaland/elsa/util"
+	"bytes"
+	"fmt"
+	"os"
 	"os/exec"
 )
 
 func ExecBuild(path string) {
 	cmd := exec.Command("go", "build", ".")
 	cmd.Dir = path
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
 	err := cmd.Run()
-	util.Check(err)
+	if err != nil {
+		fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
+		os.Exit(1)
+	}
 }
--- packager/template.go
+++ packager/template.go
@@ -6,21 +6,32 @@ import (
 	"github.com/elsaland/elsa/module"
 )
 
-const source string = `package main
+const source string = `
+package main
 
 import (
   "os"
   "github.com/elsaland/elsa/core" 
   "github.com/elsaland/elsa/module"
+  "github.com/elsaland/elsa/core/options"
 )
 
 func main() {
   snap, _ := Asset("%s")
-	toml, _ := Asset("%s")
+  toml, _ := Asset("%s")
   config, _ := module.ConfigParse(toml)
-	core.Run("elsa.js", string(snap), os.Args[1:], config, &cmd.Perms{ Fs: true })
+  env := options.Environment{
+	NoColor: config.Options.NoColor,
+	Args:    os.Args[1:],
+  }
+  opt := options.Options{
+	SourceFile: "elsa.js",
+	Source:     string(snap),
+	Perms:      &options.Perms{Fs: true},
+	Env:        env,
+  }
+  core.Run(opt)
 }
-
 `
 
 func GeneratePkgSource(path string) string {
