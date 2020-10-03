16a980d84c594c0c539fe00a3a004b6a80f8209b
divy-work <dj.srivastava23@gmail.com>
2020-10-03 22:31:04 +0530

    feat!: add uuid

--- std/async/README.md
+++ std/async/README.md
@@ -7,9 +7,9 @@ The async module has been _completely_ ported from Deno's standard modules.
 - Deferred - Creates a Promise with the `reject` and `resolve` functions.
 - Delay - Resolve a Promise after a given amount of milliseconds
 - MuxAsyncIterator - The MuxAsyncIterator class multiplexes multiple async iterators into a single
-stream. The class makes an assumption that the final result (the value returned and not
-yielded from the iterator) does not matter. If there is any result, it is
-discarded.
+  stream. The class makes an assumption that the final result (the value returned and not
+  yielded from the iterator) does not matter. If there is any result, it is
+  discarded.
 - pooledMap - Transform values from an (async) iterable into another async iterable. The
-transforms are done concurrently, with a max concurrency defined by the
-poolLimit.
+  transforms are done concurrently, with a max concurrency defined by the
+  poolLimit.
--- std/async/deffered.ts
+++ std/async/deffered.ts
@@ -5,22 +5,21 @@
 // See https://github.com/Microsoft/TypeScript/issues/15202
 // At the time of writing, the github issue is closed but the problem remains.
 export interface Deferred<T> extends Promise<T> {
-    resolve: (value?: T | PromiseLike<T>) => void;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    reject: (reason?: any) => void;
-  }
-  /** Creates a Promise with the `reject` and `resolve` functions
-   * placed as methods on the promise object itself. It allows you to do:
-   *
-   *     const p = deferred<number>();
-   *     // ...
-   *     p.resolve(42);
-   */
-  export function deferred<T>(): Deferred<T> {
-    let methods;
-    const promise = new Promise<T>((resolve, reject): void => {
-      methods = { resolve, reject };
-    });
-    return Object.assign(promise, methods) as Deferred<T>;
-  }
-  
+  resolve: (value?: T | PromiseLike<T>) => void;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  reject: (reason?: any) => void;
+}
+/** Creates a Promise with the `reject` and `resolve` functions
+ * placed as methods on the promise object itself. It allows you to do:
+ *
+ *     const p = deferred<number>();
+ *     // ...
+ *     p.resolve(42);
+ */
+export function deferred<T>(): Deferred<T> {
+  let methods;
+  const promise = new Promise<T>((resolve, reject): void => {
+    methods = { resolve, reject };
+  });
+  return Object.assign(promise, methods) as Deferred<T>;
+}
