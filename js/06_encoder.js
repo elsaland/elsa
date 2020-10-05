d3809e7fd9b94e2057b557eafdcb48967496aede
Divy Srivastava <dj.srivastava23@gmail.com>
2020-10-05 17:52:48 +0530

    feat(http/serve): implement status code and fix double free corruption (#74)

--- js/06_encoder.js
+++ js/06_encoder.js
@@ -1,22 +1,22 @@
 // Copyright 2020 elsa.land authors. All rights reserved. MIT license.
-var utf8Encodings = [
-  'utf8',
-  'utf-8',
-  'unicode-1-1-utf-8'
-];
+var utf8Encodings = ["utf8", "utf-8", "unicode-1-1-utf-8"];
 
 function TextEncoder(encoding) {
-  if (utf8Encodings.indexOf(encoding) < 0 && typeof encoding !== 'undefined' && encoding != null) {
-    throw new RangeError('Invalid encoding type. Only utf-8 is supported');
+  if (
+    utf8Encodings.indexOf(encoding) < 0 &&
+    typeof encoding !== "undefined" &&
+    encoding != null
+  ) {
+    throw new RangeError("Invalid encoding type. Only utf-8 is supported");
   } else {
-    this.encoding = 'utf-8';
-    this.encode = function(str) {
-      if (typeof str !== 'string') {
-        throw new TypeError('passed argument must be of tye string');
+    this.encoding = "utf-8";
+    this.encode = function (str) {
+      if (typeof str !== "string") {
+        throw new TypeError("passed argument must be of tye string");
       }
       var binstr = unescape(encodeURIComponent(str)),
         arr = new Uint8Array(binstr.length);
-      const split = binstr.split('');
+      const split = binstr.split("");
       for (let i = 0; i < split.length; i++) {
         arr[i] = split[i].charCodeAt(0);
       }
@@ -26,35 +26,40 @@ function TextEncoder(encoding) {
 }
 
 function TextDecoder(encoding) {
-  if (utf8Encodings.indexOf(encoding) < 0 && typeof encoding !== 'undefined' && encoding != null) {
-    throw new RangeError('Invalid encoding type. Only utf-8 is supported');
-  }
-  else {
-    this.encoding = 'utf-8';
+  if (
+    utf8Encodings.indexOf(encoding) < 0 &&
+    typeof encoding !== "undefined" &&
+    encoding != null
+  ) {
+    throw new RangeError("Invalid encoding type. Only utf-8 is supported");
+  } else {
+    this.encoding = "utf-8";
     this.decode = function (view, options) {
-      if (typeof view === 'undefined') {
-        return '';
+      if (typeof view === "undefined") {
+        return "";
       }
 
-      var stream = (typeof options !== 'undefined' && stream in options) ? options.stream : false;
-      if (typeof stream !== 'boolean') {
-        throw new TypeError('stream option must be boolean');
+      var stream =
+        typeof options !== "undefined" && stream in options
+          ? options.stream
+          : false;
+      if (typeof stream !== "boolean") {
+        throw new TypeError("stream option must be boolean");
       }
 
       if (!ArrayBuffer.isView(view)) {
-        throw new TypeError('passed argument must be an array buffer view');
+        throw new TypeError("passed argument must be an array buffer view");
       } else {
         var arr = new Uint8Array(view.buffer, view.byteOffset, view.byteLength),
           charArr = new Array(arr.length);
         for (let i = 0; i < arr.length; i++) {
           charArr[i] = String.fromCharCode(arr[i]);
         }
-        return decodeURIComponent(escape(charArr.join('')));
+        return decodeURIComponent(escape(charArr.join("")));
       }
-    }
+    };
   }
 }
-
 
 /**
  * Implementation of atob() according to the HTML and Infra specs, except that
