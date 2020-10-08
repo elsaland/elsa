28adef0df10b78595f194d29cfe2328198186f69
Erick Sosa <ericksosagarcias@gmail.com>
2020-10-07 22:14:00 -0400

    feat: testing improvements (#95)
    * feat: deepEqual function and testing improvements
    
    * update: merge to elsa master

--- js/07_testing.js
+++ js/07_testing.js
@@ -8,16 +8,44 @@ Elsa.tests = function (tests) {
   // Loop through tests and run the function passed.
   // It basically `try...catch`'s the run and determines its failure.
   // No additional helper utilities are provided.
+  let passed = 0;
+  const startTestTime = Date.now();
+
+  const red = "\u001b[31m";
+  const gree = "\u001b[32m";
+  const reset = "\u001b[0m";
+  const gray = "\u001b[38;5;8m";
+  const greenBG = "\u001b[42m";
+  const redBG = "\u001b[41m";
+
   for (let testName in tests) {
     let testAction = tests[testName];
+    const timeBeforeStart = Date.now();
     try {
       testAction();
-      console.log(`TEST ${testName} ... OK`);
+
+      console.log(
+        `TEST ${testName} ... ${greenBG} OK ${reset} ${gray}(${
+          Date.now() - timeBeforeStart
+        }ms)${reset}\n`
+      );
+
+      passed++;
     } catch (e) {
-      // Increments faliure, prints stact trace
       failures++;
-      console.error(`TEST ${testName} ... FAILED ${e}`);
+      console.error(
+        `TEST ${testName} ... ${redBG} FAILED ${reset} ${gray}(${
+          Date.now() - timeBeforeStart
+        }ms)${reset} \n ${e}`
+      );
       console.error(e.stack);
     }
   }
+  const endTestTime = Date.now() - startTestTime;
+
+  console.log(
+    `TEST results: ${passed} passed, ${failures} failed, ${
+      passed + failures
+    } total, ${gray}(${endTestTime}ms)${reset}`
+  );
 };
--- testing/tests/utils.ts
+++ testing/tests/utils.ts
@@ -6,7 +6,39 @@ export function add(a: number, b: number) {
 }
 
 export function eq(a: number | string | boolean, b: number | string | boolean) {
-  if (a !== b) {
-    throw new Error(`Assertion failed: ${a} !== ${b}`);
+  if (!deepEqual(a, b)) {
+    throw new Error(`Assertion failed: ${a} !== ${b}\n`);
+  }
+}
+
+export function deepEqual(a: any, b: any) {
+  if (Object.is(a, b)) {
+    // items are identical
+    return true;
+  } else if (
+    typeof a === "object" &&
+    a !== null &&
+    typeof b === "object" &&
+    b !== null
+  ) {
+    // items are objects - do a deep property value compare
+    // join keys from both objects together in one array
+    let keys = Object.keys(a).concat(Object.keys(b));
+    // filter out duplicate keys
+    keys = keys.filter(function (value, index, self) {
+      return self.indexOf(value) === index;
+    });
+    for (let p of keys) {
+      if (typeof a[p] === "object" && typeof b[p] === "object") {
+        if (!deepEqual(a[p], b[p])) {
+          return false;
+        }
+      } else if (a[p] !== b[p]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
   }
 }
