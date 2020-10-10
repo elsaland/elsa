437ab36d7aa87802e35f78b29212cae46aa1f261
Erick Sosa Garcia <ericksosagarcias@gmail.com>
2020-10-10 11:05:08 -0400

    feat(core/ops/env): add toObject env method

--- core/ops/env.go
+++ core/ops/env.go
@@ -1,27 +1,55 @@
 package ops
 
 import (
+	"encoding/json"
+	"os"
+	"strings"
+
 	"github.com/elsaland/elsa/util"
 	"github.com/elsaland/quickjs"
-
-	"os"
 )
 
 // Env elsa handler
 func Env(ctx *quickjs.Context, data []quickjs.Value) quickjs.Value {
 
 	// Elsa.env.get
-	if len(data) == 2 {
+	if len(data) == 2 && data[1].IsString() {
 		key := os.Getenv(data[1].String())
 		return ctx.String(key)
 	}
 
 	// Elsa.env.set
-	if len(data) == 3 {
+	if len(data) == 3 && (data[1].IsString() && data[2].IsString()) {
 		err := os.Setenv(data[1].String(), data[2].String())
 		util.Check(err)
 
 		return ctx.Null()
+	}
+
+	// Elsa.env.toObject
+	if len(data) == 2 && data[1].IsBool() {
+		getenvironment := func(envs []string, getkeyval func(item string) (key, val string)) map[string]string {
+			items := make(map[string]string)
+			for _, item := range envs {
+				key, val := getkeyval(item)
+				items[key] = val
+			}
+			return items
+		}
+
+		// get envs map
+		environment := getenvironment(os.Environ(), func(item string) (key, val string) {
+			splits := strings.Split(item, "=")
+			key = splits[0]
+			val = splits[1]
+			return
+		})
+
+		jsonEnv, err := json.Marshal(environment)
+
+		util.Check(err)
+		// convert to json string object
+		return ctx.String(string(jsonEnv))
 	}
 
 	return ctx.Null()
